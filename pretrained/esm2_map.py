a968c072a51b1180dc0e7c2dc1af30b8eaa690fd
MD. Youshuf Khan Rakib <59321214+khanonuvov@users.noreply.github.com>
2025-07-28 05:05:52 +0800

    Add files via upload

---
+++ pretrained/esm2_map.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  1 17:49:30 2025
+
+@author: Yoush
+"""
+
+import pickle
+from tqdm import tqdm
+import pickle
+import esm
+import pandas as pd
+import torch
+
+
+def load_pickle(dir):
+    with open(dir, 'rb+') as f:
+        return pickle.load(f)
+
+
+model, alphabet = esm.pretrained.esm2_t36_3B_UR50D()
+batch_converter = alphabet.get_batch_converter()
+model.eval()
+
+def get_esm_contact_map(model, df_dir, db_name, max_length=1200):
+    df = pd.read_csv(df_dir)    
+    prot_data = []
+    for prot_id, seq in zip(df['prot_id'], df['prot_seq']):
+        truncated_seq = seq[:max_length]
+        prot_data.append((prot_id, truncated_seq))
+    
+    target_graph = {}
+    length_target = {}
+
+    for prot_id, seq in tqdm(prot_data):
+        batch_labels, batch_strs, batch_tokens = batch_converter([(prot_id, seq)])
+        batch_lens = (batch_tokens != alphabet.padding_idx).sum(1)
+        
+        with torch.no_grad():
+            results = model(batch_tokens, repr_layers=[33], return_contacts=True)
+
+        contact_map = results["contacts"][0].numpy()
+
+        target_graph[prot_id] = contact_map
+        length_target[prot_id] = len(seq)
+    
+    dump_data = {
+        "dataset": db_name,
+        "contact_map": target_graph,
+        "length_dict": length_target
+    }
+    
+    output_path = f'./GSIK-DTA/pretrained/{db_name}/{db_name}_esm2_contact_map.pkl'
+    with open(output_path, 'wb') as f:
+        pickle.dump(dump_data, f)
+
+    print(f"✅ Saved contact maps for {db_name}.")
+        
+
+db_names = ['davis', 'kiba', 'metz']
+df_dirs = [r'./GSIK-DTA/datasets/davis/davis_prots.csv', r'./GSIK-DTA/datasets/kiba/kiba_prots.csv', r'./GSIK-DTA/datasets/metz/metz_prots.csv']
+
+for i in range(0,3):
+    print(f'Compute {df_dirs[i]} protein pretrain feature by esm2.')
+    get_esm_contact_map(model, df_dirs[i], db_names[i])
+    
---
+++ pretrained/esmC_pretraiend.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  1 17:49:30 2025
+@author: Yoush
+"""
+import pickle
+from tqdm import tqdm
+import pandas as pd
+import torch
+
+# Import ESMC model and ESMProtein wrapper
+from esm.models.esmc import ESMC
+from esm.sdk.api import ESMProtein, LogitsConfig
+
+# Load model
+model = ESMC.from_pretrained("esmc_600m").to("cuda").eval()
+
+# Set max sequence length
+MAX_SEQ_LENGTH = 1200  # ESM-C supports long sequences
+
+def get_esmc_pretrain(model, df_dir, db_name):
+    df = pd.read_excel(df_dir)
+    
+    emb_dict, emb_mat_dict, length_target = {}, {}, {}
+
+    for prot_id, seq in tqdm(zip(df['prot_id'], df['prot_seq']), total=len(df)):
+        seq = seq[:MAX_SEQ_LENGTH]  # truncate if needed
+        protein = ESMProtein(sequence=seq)
+
+        with torch.no_grad():
+            # Use LogitsConfig to request embeddings
+            logits_output = model.logits(
+                model.encode(protein),
+                LogitsConfig(sequence=True, return_embeddings=True)
+            )
+
+        reps = logits_output.embeddings[0]
+        reps = reps.cpu().numpy()
+        
+        emb_mat_dict[prot_id] = reps
+        emb_dict[prot_id] = reps.mean(axis=0)
+        length_target[prot_id] = len(seq)
+
+    # Save embeddings to pickle
+    with open(f'./GSIK-DTA/pretrained/{db_name}/{db_name}_esmc_pretrain.pkl', 'wb') as f:
+        pickle.dump({
+            "dataset": db_name,
+            "vec_dict": emb_dict,
+            "mat_dict": emb_mat_dict,
+            "length_dict": length_target
+        }, f)
+    
+    print(f"✅ Saved {db_name} ESM-C features.")
+
+
+db_names = ['davis', 'kiba', 'metz']
+df_dirs = [r'./GSIK-DTA/datasets/davis/davis_prots.csv', r'./GSIK-DTA/datasets/kiba/kiba_prots.csv', r'./GSIK-DTA/datasets/metz/metz_prots.csv']
+
+for i in range(0,3):
+    print(f'Compute {df_dirs[i]} protein pretrain feature by esm-c.')
+    get_esm_contact_map(model, df_dirs[i], db_names[i])

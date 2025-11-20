b45823e3d273b1a5e2737d04072216b36dd33dfd
MD. Youshuf Khan Rakib <59321214+khanonuvov@users.noreply.github.com>
2025-11-20 20:50:28 +0800

    Update collate function in DataLoader for predictions

--- code/pred.py
+++ code/pred.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 from rdkit import Chem
 from model import MODEL as Model
 from hyperparameter import HyperParameter
-from MyDataset import CustomDataSet, my_collate_fn
+from MyDataset import CustomDataSet, pred_my_collate_fn
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -71,7 +71,7 @@ contact_map = load_pickle(hp.contact_map)
 test_dir = './KANPM-DTA/datasets/test/test.csv'
 test_df = pd.read_csv(test_dir)
 test_set = CustomDataSet(test_df, hp)
-test_dataset_load = DataLoader(test_set, batch_size=hp.Batch_size, shuffle=False, drop_last=False, num_workers=8, collate_fn=lambda x: my_collate_fn(x, device, hp, drug_df, prot_df, mol2vec_dict, protvec_dict, contact_map))
+test_dataset_load = DataLoader(test_set, batch_size=hp.Batch_size, shuffle=False, drop_last=False, num_workers=8, collate_fn=lambda x: pred_my_collate_fn(x, device, hp, drug_df, prot_df, mol2vec_dict, protvec_dict, contact_map))
  
 
 predModel = nn.DataParallel(Model(hp, device))

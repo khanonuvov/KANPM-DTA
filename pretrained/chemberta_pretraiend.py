d119349bae041fe41e25da1fe0cacb9898878a69
MD. Youshuf Khan Rakib <59321214+khanonuvov@users.noreply.github.com>
2026-01-13 17:12:16 +0800

    Update chemberta_pretraiend.py

--- pretrained/chemberta_pretraiend.py
+++ pretrained/chemberta_pretraiend.py
@@ -53,4 +53,5 @@ df_dirs = [r'./KANPM-DTA/datasets/davis/davis_drugs.csv', r'./KANPM-DTA/datasets
 for i in range(0,3):
     print(f'Compute {df_dirs[i]} drug pretrain feature by Chemberta-2.')
 
-    get_esm_contact_map(model, df_dirs[i], db_names[i])
+    get_chem_pretrain(model, df_dirs[i], db_names[i])
+
